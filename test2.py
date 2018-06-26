b6afb6b537c1d6b00c9063759b216dacbb509b4d
bijing <jbi@3ictrl.com>
2018-06-26 10:42:52 +0800

    增加注释

--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+# 单行注释使用#
 # A comment, this is so you can read your program later.
 # Anything after the # is ignored by python.
 print "I could have code like this."# and the comment after is ignored
